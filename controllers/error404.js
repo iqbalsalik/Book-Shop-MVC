daec99e94829c0c00383b96687e35b1d2bec4eae
iqbalsalik <sisalik84@gmail.com>
2023-04-17 01:18:57 +0530

    Dynamic Data

--- controllers/error404.js
+++
@@ -1,3 +0,0 @@
-exports.pageError = (req, res, next) => {
-    res.status(404).render('404', { pageTitle: 'Page Not Found' });
-  }
--- routes/shop.js
+++ routes/shop.js
@@ -2,13 +2,20 @@ const path = require('path');
 
 const express = require('express');
 
-const productController  = require("../controllers/products")
-
-const rootDir = require('../util/path');
-const adminData = require('./admin');
+const shopController = require('../controllers/shop');
 
 const router = express.Router();
 
-router.get('/',productController.getProduct);
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+router.get("/products/:productId",shopController.getProductDetails)
+
+router.get('/cart', shopController.getCart);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
