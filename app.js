106582a9b8945b4e0bd8650dccaf5254cf4b462a
iqbalsalik <sisalik84@gmail.com>
2023-04-26 16:03:18 +0530

    sequelize.sync

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 
-const db = require("./util/database")
+const sequelize = require("./util/database")
 
 const app = express();
 
@@ -23,5 +23,11 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
+
+sequelize.sync()
+.then(result=>{
+    console.log(result);
+})
+.catch(err=>console.log(err))
 
 app.listen(3000);
